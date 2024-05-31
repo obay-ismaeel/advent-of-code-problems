9057dda3f14030b477a82b57a46f0f9cc8c0f98e
Obay Ismaeel <obayhany@gmail.com>
2024-05-31 15:06:46 +0300

    Add project files.

---
+++ Problem1/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+long sum = 0;
+
+foreach(string line in input)
+{
+    int first = -1, last = -1;
+    
+    foreach(char ch in line)
+    {
+        if (char.IsDigit(ch) && first is -1)
+        {
+            first = ch - '0';
+            last = ch - '0';
+        }
+        else if (char.IsDigit(ch))
+        {
+            last = ch - '0';
+        }
+    }
+
+    sum += first * 10 + last;
+}
+
+Console.WriteLine($"Solution: {sum}");
---
+++ Problem2/Program.cs
@@ -0,0 +1,89 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+Console.WriteLine($"Part one solution: {SolvePartOne(lines)}");
+Console.WriteLine($"Part two solution: {SolvePartTwo(lines)}");
+
+static int SolvePartTwo(string[] lines)
+{
+    int answer = 0;
+
+    var games = lines.Select(x => x.Split(':')[1]).Select(x => x.Split(';'));
+
+    foreach (var game in games)
+    {
+        int minRed = 0, minGreen = 0, minBlue = 0;
+
+        foreach (var pick in game)
+        {
+            var ballColors = pick.Split(',').Select(x => x.Trim());
+
+            foreach (var colorShows in ballColors)
+            {
+                var number = int.Parse(colorShows.Split(' ')[0]);
+                var color = colorShows.Split(' ')[1];
+
+                if(color is "red")
+                {
+                    minRed = int.Max(minRed, number);
+                }else if (color is "green")
+                {
+                    minGreen = int .Max(minGreen, number);
+                }else if (color is "blue")
+                {
+                    minBlue = int.Max(minBlue, number);
+                }
+            }
+        }
+
+        answer += minBlue * minRed * minGreen;
+    }
+
+    return answer;
+}
+
+static int SolvePartOne(string[] lines)
+{
+    int answer = 0;
+
+    var games = lines.Select(x => x.Split(':')[1]).Select(x => x.Split(';'));
+
+    var gameNumber = 1;
+
+    foreach (var game in games)
+    {
+        bool valid = true;
+
+        foreach (var pick in game)
+        {
+            var ballColors = pick.Split(',').Select(x => x.Trim());
+
+            foreach (var colorShows in ballColors)
+            {
+                var number = int.Parse(colorShows.Split(' ')[0]);
+                var color = colorShows.Split(' ')[1];
+
+                if (number > MaxColorNumber(color))
+                {
+                    valid = false;
+                }
+            }
+        }
+
+        if (valid)
+            answer += gameNumber;
+        gameNumber++;
+    }
+
+    return answer;
+}
+
+static int MaxColorNumber(string color)
+{
+    if (color is "red")
+        return 12;
+    if (color is "green")
+        return 13;
+    if (color is "blue")
+        return 14;
+    throw new ArgumentException();
+}
