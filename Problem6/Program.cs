467b23e4d88629db9a60821b14cd1e354d6e762c
Obay Ismaeel <obayhany@gmail.com>
2024-06-09 02:42:41 +0300

    Solved part 2 of problem 6

--- Problem6/Program.cs
+++ Problem6/Program.cs
@@ -3,6 +3,7 @@
 var input = File.ReadAllLines("input.txt");
 
 Console.WriteLine($"Part one solution: {SolvePartOne(input)}");
+Console.WriteLine($"Part two solution: {SolvePartTwo(input)}");
 
 int SolvePartOne(string[] input)
 {
@@ -35,6 +36,21 @@ int SolvePartOne(string[] input)
     return answer;
 }
 
+int SolvePartTwo(string[] input)
+{
+    var time = GetNumber(input[0]);
+    var distance = GetNumber(input[1]);
+
+    var delta = Math.Pow(time, 2) - (4 * 1 * distance);
+
+    var x1 = (-time + Math.Sqrt(delta)) / 2;
+    var x2 = (-time - Math.Sqrt(delta)) / 2;
+
+    return CalcWholeNumbersCountBetween(x1, x2);
+}
+
 List<int> GetNumbersList(string line) => Regex.Split(line.Split(':')[1].Trim(), @"\s+").Select(x => int.Parse(x)).ToList();
+
+long GetNumber(string line) => long.Parse(Regex.Replace(line.Split(':')[1], @"\s+", ""));
 
 int CalcWholeNumbersCountBetween(double x1, double x2) => (int)Math.Abs(Math.Ceiling(x1) - Math.Floor(x2)) - 1;
